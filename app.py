6e84c45c157a7ecff2572ea6777cd36e72baa0a0
Leonel <leonelmandarino@gmail.com>
2021-07-23 14:13:34 -0300

    Add logging and proyect refactor

--- app.py
+++ app.py
@@ -1,68 +1,76 @@
 import cv2
 import httpx
 from datetime import datetime
+import logging
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
 BASE_URL = "http://127.0.0.1:8000"
 HAS_CONNECTION = True
 
 try:
     EXPERIMENT_ID = httpx.get(f"{BASE_URL}/experiments/last").json()["id"]
+    logging.info(f"Connected to {BASE_URL}")
 except httpx.ConnectError:
+    logging.info("Couldn't connect to remote host, running in offline mode")
     HAS_CONNECTION = False
 
 
-face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("./haarcascade_eye.xml")
+def post_event(detected):
+    if not HAS_CONNECTION:
+        return
+
+    status = "I see you" if detected else "I don't see you"
+    event = {
+        "timestamp": datetime.now().isoformat(),
+        "status": status,
+        "name": "CAMERA",
+        "experiment": EXPERIMENT_ID,
+    }
+    httpx.post(f"{BASE_URL}/events/", data=event)
+    logging.debug(
+        f"POST event with status and experiment id: {status} - {EXPERIMENT_ID}"
+    )
+
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise RuntimeError("Couldn't open camera, check if it's connected")
 
 
-async def post_event(detected):
-    event = {
-        "timestamp": datetime.now().isoformat(),
-        "status": "I see you" if detected else "I don't see you",
-        "name": "CAMERA",
-        "experiment": EXPERIMENT_ID,
-    }
-    async with httpx.AsyncClient() as client:
-        client.post(f"{BASE_URL}/events/", data=event)
+face_cascade = cv2.CascadeClassifier(
+    "./haarcascade_models/haarcascade_frontalface_default.xml"
+)
+eye_cascade = cv2.CascadeClassifier("./haarcascade_models/haarcascade_eye.xml")
 
-
-post_event(detected=True)
 face_in_last_iteration = False
-
 while True:
     # Capture frames
-    ret, frame = cap.read()
+    _, img = cap.read()
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame. Exiting ...")
-        break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.08, 7, minSize=(100, 100))
 
-    if faces and not face_in_last_iteration:
+    if len(faces) != 0 and not face_in_last_iteration:
         post_event(detected=True)
         face_in_last_iteration = True
-    elif not faces and face_in_last_iteration:
+    elif len(faces) == 0 and face_in_last_iteration:
         post_event(detected=False)
         face_in_last_iteration = False
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = frame[y : y + h, x : x + w]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_color = img[y : y + h, x : x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.08, 7, minSize=(30, 30))
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-    cv2.imshow("img", frame)
-    if cv2.waitKey(1) == ord("q"):
+    cv2.imshow("img", img)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
         break
 
 cap.release()
